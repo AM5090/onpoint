e2221ffdf7293cefa328fb6f17b7f3f450b70549
AM5090 <armen.melkonyan.93@inbox.ru>
2022-03-03 23:24:08 +0300

    Промежуточное сохранение

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
 const track = document.querySelector('.slide_track');
+const slideTrack = document.querySelectorAll('.slide_track');
 
 let x1 = null;
 let x2 = null;
 let moveInterval = null;
+let trackPosition = Math.floor(Number(getComputedStyle(track).left.slice(0, -2)));
 
 document.addEventListener('touchstart' , (event) => {
     const firstTouch = event.touches[0];
@@ -11,13 +13,24 @@ document.addEventListener('touchstart' , (event) => {
 
 document.addEventListener('touchmove', (event) => {
     const moveTouch = event.touches[0];
-    x2 = moveTouch.clientX;
-    moveInterval = Math.floor(x2 - x1);
     trackPosition = Math.floor(Number(getComputedStyle(track).left.slice(0, -2)));
-    console.log("moveInterval", moveInterval);
-    console.log("trackPosition", trackPosition);
-    //track.style.left = Math.floor(trackPosition + moveInterval) + 'px';
-    if (moveInterval < trackPosition) {
-        track.style.left = "-1024px"
+
+    x2 = moveTouch.clientX;
+    moveInterval = Math.floor((x2 - x1) + trackPosition);
+
+    if (moveInterval >= 0) {
+        moveInterval = 0;
+    } else if (moveInterval <= -2048) {
+        moveInterval = -2048;
     }
+
+    if (moveInterval <= -60 && moveInterval >= -1024) {
+        moveInterval = -1024;
+    } else if (moveInterval <= -1084 && moveInterval >= -2048) {
+        moveInterval = -2048;
+    }
+
+    track.style.left = moveInterval + 'px';
+    console.log('moveInterval', moveInterval);
+
 });
