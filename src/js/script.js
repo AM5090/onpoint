bdea8d14a0caf42615e0070ecc590924b9f173e8
AM5090 <armen.melkonyan.93@inbox.ru>
2022-03-03 21:19:31 +0300

    Промежуточное сохранение

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,6 @@ const track = document.querySelector('.slide_track');
 let x1 = null;
 let x2 = null;
 let moveInterval = null;
-/*let trackPosition = getComputedStyle(track).left;
-console.log("trackPosition", trackPosition);*/
 
 document.addEventListener('touchstart' , (event) => {
     const firstTouch = event.touches[0];
@@ -14,9 +12,12 @@ document.addEventListener('touchstart' , (event) => {
 document.addEventListener('touchmove', (event) => {
     const moveTouch = event.touches[0];
     x2 = moveTouch.clientX;
-    moveInterval = x2 - x1;
-    trackPosition = getComputedStyle(track).left.slice(0, -2);
-    console.log(Number(trackPosition));
-    track.style.left = moveInterval + 'px';
-    
+    moveInterval = Math.floor(x2 - x1);
+    trackPosition = Math.floor(Number(getComputedStyle(track).left.slice(0, -2)));
+    console.log("moveInterval", moveInterval);
+    console.log("trackPosition", trackPosition);
+    //track.style.left = Math.floor(trackPosition + moveInterval) + 'px';
+    if (moveInterval < trackPosition) {
+        track.style.left = "-1024px"
+    }
 });
