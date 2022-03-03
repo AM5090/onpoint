23399abfde424ba98f2b60ca6ec1d6d0a8ca01ee
AM5090 <armen.melkonyan.93@inbox.ru>
2022-03-03 18:34:21 +0300

    Завершил все три окна начал писать код

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+let x = null;
+let y = null;
+
+document.addEventListener('touchstart' , (event) => {
+    const firstTouch = event.touches[0];
+    console.log(firstTouch);
+    /*x = firstTouch
+    y = */
+});
